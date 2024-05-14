c8ae9b0bdccd20a1fc277ad93d1a393f193eb789
phuongnguyen172 <eiphuong17@gmail.com>
2024-05-14 14:31:59 +0700

    Delete outputs.tf files

--- terraform/demo-project/global/networking/route53_records/main.tf
+++ terraform/demo-project/global/networking/route53_records/main.tf
@@ -16,38 +16,32 @@ data "aws_route53_zone" "poeta_click" {
   name = "poeta.click"
 }
 
-data "terraform_remote_state" "elastic_ips" {
-  backend = "local"
-
-  config = {
-    path = "/home/phuongnguyen/D/!Poeta/Study/Infrastructure/terraform/projects/demo/mgmt/networking/elastic_ips/terraform.tfstate"
+data "aws_eip" "jenkins" {
+  tags = {
+    Name = "EIP_Jenkins"
   }
-}
-
-data "aws_lb" "k8s_load_balancer" {
-  name = "LB-Kubernetes"
 }
 
 resource "aws_route53_record" "jenkins" {
   zone_id = data.aws_route53_zone.poeta_click.id
   type    = "A"
   name    = "jenkins"
-  records = [data.terraform_remote_state.elastic_ips.outputs.eip_Jenkins_public_ip]
+  records = [data.aws_eip.jenkins.public_ip]
   ttl     = 600
 }
 
-resource "aws_route53_record" "goal" {
-  zone_id = data.aws_route53_zone.poeta_click.id
-  type = "CNAME"
-  name = "goal"
-  records = [data.aws_lb.k8s_load_balancer.dns_name]
-  ttl = 600
-}
+# resource "aws_route53_record" "goal" {
+#   zone_id = data.aws_route53_zone.poeta_click.id
+#   type = "CNAME"
+#   name = "goal"
+#   records = [data.aws_lb.k8s_load_balancer.dns_name]
+#   ttl = 600
+# }
 
-resource "aws_route53_record" "tkb" {
-  zone_id = data.aws_route53_zone.poeta_click.id
-  type = "CNAME"
-  name = "tkb"
-  records = [data.aws_lb.k8s_load_balancer.dns_name]
-  ttl = 600
-}
+# resource "aws_route53_record" "tkb" {
+#   zone_id = data.aws_route53_zone.poeta_click.id
+#   type = "CNAME"
+#   name = "tkb"
+#   records = [data.aws_lb.k8s_load_balancer.dns_name]
+#   ttl = 600
+# }
